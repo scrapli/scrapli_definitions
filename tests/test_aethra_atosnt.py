fed75213c2e27c4f18e12eb969e08d45135b1880
Carl Montanari <carl.r.montanari@gmail.com>
2025-06-20 18:21:30 +0200

    test: add aethra atosnt test

---
+++ tests/test_aethra_atosnt.py
@@ -0,0 +1,60 @@
+import re
+
+import pytest
+
+PROMPTS = (
+    (
+        "simple",
+        "exec",
+        "HOSTNAME>>",
+    ),
+    (
+        "simple-underscores-dashes",
+        "exec",
+        "SCRAPLI_AETHRA-ATOSNT_RTR>>",
+    ),
+    (
+        "simple-numbers",
+        "exec",
+        "SCRAPLI-1234>>",
+    ),
+)
+
+
+@pytest.mark.parametrize(
+    argnames=(
+        "_",
+        "__",
+        "prompt",
+    ),
+    argvalues=PROMPTS,
+    ids=[case[0] for case in PROMPTS],
+)
+def test_global_prompt_pattern(_, __, prompt, d):
+    assert (
+        re.search(pattern=d.prompt_pattern, string=prompt, flags=re.MULTILINE | re.IGNORECASE)
+        is not None
+    )
+
+
+@pytest.mark.parametrize(
+    argnames=(
+        "_",
+        "mode",
+        "prompt",
+    ),
+    argvalues=PROMPTS,
+    ids=[case[0] for case in PROMPTS],
+)
+def test_mode_prompt_patterns(_, mode, prompt, d):
+    m = next((found_mode for found_mode in d.modes if found_mode.name == mode), None)
+
+    assert m is not None
+
+    if m.prompt_exact:
+        assert prompt in m.prompt_exact
+    else:
+        assert (
+            re.search(pattern=m.prompt_pattern, string=prompt, flags=re.MULTILINE | re.IGNORECASE)
+            is not None
+        )
