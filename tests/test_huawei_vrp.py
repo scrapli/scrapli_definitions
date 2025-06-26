14d08a744a9da8723d1ceffe3b77c0f4d4b810e9
Carl Montanari <carl.r.montanari@gmail.com>
2025-06-26 13:59:45 +0100

    feat: add huawei vrp

---
+++ tests/test_huawei_vrp.py
@@ -0,0 +1,100 @@
+import re
+
+import pytest
+
+PROMPTS = (
+    (
+        "simple",
+        "privileged_exec",
+        "<SCRAPLI_HUAWEI-VRP_TEST_SW1>",
+    ),
+    (
+        "hrp-main",
+        "privileged_exec",
+        "HRP_M<SCRAPLI_HUAWEI-VRP_TEST_SW1>",
+    ),
+    (
+        "hrp-secondary",
+        "privileged_exec",
+        "HRP_S<SCRAPLI_HUAWEI-VRP_TEST_SW1>",
+    ),
+    (
+        "hrp-active",
+        "privileged_exec",
+        "HRP_A<SCRAPLI_HUAWEI-VRP_TEST_SW1>",
+    ),
+    (
+        "simple",
+        "configuration",
+        "[SCRAPLI_HUAWEI-VRP_TEST_SW1]",
+    ),
+    (
+        "hrp-main",
+        "configuration",
+        "HRP_M[SCRAPLI_HUAWEI-VRP_TEST_SW1]",
+    ),
+    (
+        "hrp-secondary",
+        "configuration",
+        "HRP_S[SCRAPLI_HUAWEI-VRP_TEST_SW1]",
+    ),
+    (
+        "hrp-active",
+        "configuration",
+        "HRP_A[SCRAPLI_HUAWEI-VRP_TEST_SW1]",
+    ),
+    (
+        "interface",
+        "configuration",
+        "[SCRAPLI_HUAWEI-VRP_TEST_SW1-GigabitEthernet0/0/1]",
+    ),
+    (
+        "tilde",
+        "configuration",
+        "[~SCRAPLI_HUAWEI-VRP_TEST_SW1-GigabitEthernet0/0/1]",
+    ),
+    (
+        "asterisk",
+        "configuration",
+        "[*SCRAPLI_HUAWEI-VRP_TEST_SW1-GigabitEthernet0/0/1]",
+    ),
+)
+
+
+@pytest.mark.parametrize(
+    argnames=(
+        "_",
+        "__",
+        "prompt",
+    ),
+    argvalues=PROMPTS,
+    ids=[case[0] for case in PROMPTS],
+)
+def test_global_prompt_pattern(_, __, prompt, d):
+    assert (
+        re.search(pattern=d.prompt_pattern, string=prompt, flags=re.MULTILINE | re.IGNORECASE)
+        is not None
+    )
+
+
+@pytest.mark.parametrize(
+    argnames=(
+        "_",
+        "mode",
+        "prompt",
+    ),
+    argvalues=PROMPTS,
+    ids=[case[0] for case in PROMPTS],
+)
+def test_mode_prompt_patterns(_, mode, prompt, d):
+    m = next((found_mode for found_mode in d.modes if found_mode.name == mode), None)
+
+    assert m is not None
+
+    if m.prompt_exact:
+        assert prompt in m.prompt_exact
+    else:
+        assert (
+            re.search(pattern=m.prompt_pattern, string=prompt, flags=re.MULTILINE | re.IGNORECASE)
+            is not None
+        )
